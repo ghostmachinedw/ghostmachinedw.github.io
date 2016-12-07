e8a7886042b32410d05480a625a0ca615de97643
Vincent Tran <vincent.tran@linux2.cs.tamu.edu>
2016-12-06 19:19:31 -0600

    added stuff

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 50) {
+      $('header').addClass('scrolled');
+    } else {
+      $('header').removeClass('scrolled');
+    };
+  });
+});
